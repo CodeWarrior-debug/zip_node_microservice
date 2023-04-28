94c07eeb8240d291b2cbc22408857821cc796e73
James Jordan <jpjordan1987@gmail.com>
2023-04-28 16:14:36 -0400

    hi!

---
+++ controller.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var properties = required('./package.json');
+var distance = required('./service/distance');
+
+var controllers = {
+    about: function(req, res){
+        var aboutInfo = {name:properties.name, version: properties.version}
+        res.json(aboutInfo), 
+    get_distance: function(req, res){
+        distance.find(req,res,function(err,dist){
+            if(err) res.send(err); res.json(dist);
+        })
+    }
+    }
+}
+
+module.exports = controllers;
---
+++ server.js
@@ -0,0 +1,5 @@
+var express = require('express');
+var app = express
+var port = process.env.PORT || 3000;
+var routes = require ('./api/routes'); routes(app);
+app.listen(port,function(){ console.log(' Server Started on port: ' + port)});
